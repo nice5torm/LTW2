2b9dd7d293b18ef0c6a5736d0161127a8898db40
nice5torm <marianadguimaraes@hotmail.com>
2017-12-15 02:01:03 +0000

    database fixe

--- database/database.sql
+++ database/database.sql
@@ -16,18 +16,16 @@ CREATE TABLE toDoList
 (
 	idList INTEGER PRIMARY KEY AUTOINCREMENT,
 	creator INTEGER NOT NULL,
-	assignedTo INTEGER NOT NULL
-	title TEXT NOT NULL, 
+	title TEXT NOT NULL,
 	category TEXT NOT NULL
 	FOREIGN KEY (creator) REFERENCES user (username)ON DELETE CASCADE
 );
-
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (1,'user3','user3', 'list1', 'category1'); 
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (2,'user5','user5', 'list2', 'category1'); 
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (3,'user1','user1', 'list3', 'category1'); 
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (4,'user2','user2', 'list4', 'category2'); 
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (5,'user4','user4', 'list5', 'category2'); 
-INSERT INTO toDoList(idList, creator, assignedTo, title, category) VALUES (6,'user2','user2', 'list6', 'category2'); 
+INSERT INTO toDoList(idList, creator, title) VALUES (1,'user3','list1');
+INSERT INTO toDoList(idList, creator, title) VALUES (2,'user5','list2');
+INSERT INTO toDoList(idList, creator, title) VALUES (3,'user1','list3');
+INSERT INTO toDoList(idList, creator, title) VALUES (4,'user2','list4');
+INSERT INTO toDoList(idList, creator, title) VALUES (5,'user4','list5');
+INSERT INTO toDoList(idList, creator, title) VALUES (6,'user2','list6');
 
 CREATE TABLE itemList
 (
@@ -36,26 +34,28 @@ CREATE TABLE itemList
 	item TEXT NOT NULL, 
 	done BOOLEAN NOT NULL DEFAULT FALSE,
 	dueDate TEXT NOT NULL
+	assignedTo INTEGER NOT NULL
+
 	FOREIGN KEY (idList) REFERENCES todoList(idList)ON DELETE CASCADE
 );
 
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (1,1,'item1',1, '2018-12-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (2,1,'item2',0, '2018-12-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (3,1,'item3',0, '2018-12-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (4,2,'item1',0, '2018-12-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (5,2,'item2',0, '2018-12-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (6,2,'item3',1, '2018-12-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (7,2,'item4',0, '2018-11-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (8,2,'item5',1, '2018-11-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (9,2,'item6',1, '2018-11-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (10,3,'item1',0, '2018-11-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (11,3,'item2',0, '2018-11-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (12,3,'item3',0, '2018-11-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (13,3,'item4',0, '2018-10-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (14,3,'item5',1, '2018-10-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (15,4,'item1',0, '2018-10-31');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (16,4,'item2',0, '2018-10-01');
-INSERT INTO itemList(idItem, idList, item, done, dueDate) VALUES (17,5,'item1',0, '2018-10-01');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (1,1,'item1',1, '2018-12-31','user1');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (2,1,'item2',0, '2018-12-31','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (3,1,'item3',0, '2018-12-31','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (4,2,'item1',0, '2018-12-01','user3');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (5,2,'item2',0, '2018-12-01','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (6,2,'item3',1, '2018-12-01','user1');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (7,2,'item4',0, '2018-11-31','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (8,2,'item5',1, '2018-11-31','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (9,2,'item6',1, '2018-11-31','user4');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (10,3,'item1',0, '2018-11-01','user3');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (11,3,'item2',0, '2018-11-01','user5');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (12,3,'item3',0, '2018-11-01','user2');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (13,3,'item4',0, '2018-10-31','user1');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (14,3,'item5',1, '2018-10-31','user5');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (15,4,'item1',0, '2018-10-31','user1');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (16,4,'item2',0, '2018-10-01','user4');
+INSERT INTO itemList(idItem, idList, item, done, dueDate,assignedTo) VALUES (17,5,'item1',0, '2018-10-01','user3');
 
 CREATE TABLE members
 (
